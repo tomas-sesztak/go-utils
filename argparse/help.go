f863b141171079f98b0089f2e186044a6aca806c
Tomas Sesztak <tsesztak@gmail.com>
2024-11-02 23:07:42 +0100

    refactor

--- argparse/help.go
+++ argparse/help.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 )
 
+// show the standard help/usage message
 func Help() {
 	fmt.Fprintf(os.Stderr, "Usage of %s", os.Args[0])
 	flag.PrintDefaults()
-	os.Exit(1)
 }
--- argparse/parseArgs.go
+++ argparse/parseArgs.go
@@ -2,6 +2,7 @@ package argparse
 
 import (
 	"flag"
+	"os"
 )
 
 // Parses args/flags using flag.Parse() and shows services.Help() if requirements are not met
@@ -9,8 +10,10 @@ func ParseArgs(needArgs bool, needFlags bool) {
 	flag.Parse()
 	if needArgs && flag.NArg() == 0 {
 		Help()
+		os.Exit(1)
 	}
 	if needFlags && flag.NFlag() == 0 {
 		Help()
+		os.Exit(1)
 	}
 }
